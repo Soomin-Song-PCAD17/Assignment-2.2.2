d83490dd59235b66598b516e0b9e5e035a7cc664
Soomin Song <song.soomin.418@outlook.com>
2025-04-14 14:52:49 -0700

    allows Add() to add two doubles

--- Maths.cs
+++ Maths.cs
@@ -5,6 +5,10 @@ class Maths
         return num1+num2;
     }
 
+    public static double Add(double num1, double num2) {
+        return num1+num2;
+    }
+
     public static decimal Add(decimal num1, decimal num2, decimal num3){
         return num1+num2+num3;
     }
