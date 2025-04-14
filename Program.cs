df719b4bc807122054a7d5652500d45f61ee74ef
Soomin Song <song.soomin.418@outlook.com>
2025-04-14 13:20:57 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using static Maths.Maths;
+
+Console.WriteLine(Add(1, 2));
+Console.WriteLine(Add(1.1m, 2.2m, 3.3m));
+Console.WriteLine(Multiply(11.1f, 2.22f));
+Console.WriteLine(Multiply(1.0f, 2.0f, 3.5f));
